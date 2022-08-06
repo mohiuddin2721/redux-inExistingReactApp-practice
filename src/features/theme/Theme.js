425daade9810e923007ff5813234a96107c56ce2
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-07 01:26:23 +0600

    theme component created for another practice

---
+++ src/features/theme/Theme.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Theme = () => {
+    return (
+        <div>
+            <input type="text" className='textBox' />
+        </div>
+    );
+};
+
+export default Theme;
